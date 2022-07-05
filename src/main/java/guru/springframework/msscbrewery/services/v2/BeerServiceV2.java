29dc8fe7be6be46090a21572a653d3a8859dc361
Matheus Emanuel Carraro de Souza <42457356+MatheusCarraro@users.noreply.github.com>
2022-07-05 10:52:30 -0300

    fix: including annotation

--- src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceV2.java
+++ src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceV2.java
@@ -1,8 +1,10 @@
 package guru.springframework.msscbrewery.services.v2;
 
 import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+
 
 public interface BeerServiceV2 {
     BeerDtoV2 getBeerById(UUID beerId);
